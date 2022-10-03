36bd1fff99865c194e8681d553988e2400efd95e
rphinizy <robinphinizy@yahoo.com>
2022-10-02 21:32:15 -0400

    Add project files.

---
+++ MovieList/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace MovieList.Models
+{
+    public class Genre
+    {
+        public string GenreId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MovieList/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        /* Movie Id, Name, Year, and Rating properties same as before */
+
+        [Required(ErrorMessage ="Please nter a genre.")]
+        public string GenreId { get; set; }
+        public Genre Genre { get; set; }
+        // EF will instruct the database to automatically generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2050, ErrorMessage = "Year must be between 1889 and now.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
+    }
+}
